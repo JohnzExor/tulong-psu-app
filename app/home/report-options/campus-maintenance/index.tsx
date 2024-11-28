256c1d6b42c7bbdc3856a56f6889545493245bb0
JohnzExor <jimenojohnzyll@gmail.com>
2024-11-28 20:08:08 +0800

    added gluestack ui

--- app/home/report-options/campus-maintenance/index.tsx
+++ app/home/report-options/campus-maintenance/index.tsx
@@ -1,10 +1,21 @@
-import { Text, View } from "react-native";
+import { Controller, useForm } from "react-hook-form";
+import { SafeAreaView, Text, TextInput, View } from "react-native";
 
 const CampusMaintenanceRequest = () => {
+  const { control } = useForm();
   return (
-    <View>
-      <Text>Campus Maintenance</Text>
-    </View>
+    <SafeAreaView>
+      <View>
+        <Controller
+          control={control}
+          render={({ field }) => (
+            <TextInput {...field} placeholder="Report name" />
+          )}
+          name="report"
+          rules={{ required: "You must enter your name" }}
+        />
+      </View>
+    </SafeAreaView>
   );
 };
 
