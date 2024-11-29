546a480585370abc71e028877e9e762b320ee5eb
JohnzExor <jimenojohnzyll@gmail.com>
2024-11-29 20:21:08 +0800

    Revert "added gluestack ui"
    This reverts commit 256c1d6b42c7bbdc3856a56f6889545493245bb0.

--- app/home/report-options/campus-maintenance/index.tsx
+++ app/home/report-options/campus-maintenance/index.tsx
@@ -1,21 +1,10 @@
-import { Controller, useForm } from "react-hook-form";
-import { SafeAreaView, Text, TextInput, View } from "react-native";
+import { Text, View } from "react-native";
 
 const CampusMaintenanceRequest = () => {
-  const { control } = useForm();
   return (
-    <SafeAreaView>
-      <View>
-        <Controller
-          control={control}
-          render={({ field }) => (
-            <TextInput {...field} placeholder="Report name" />
-          )}
-          name="report"
-          rules={{ required: "You must enter your name" }}
-        />
-      </View>
-    </SafeAreaView>
+    <View>
+      <Text>Campus Maintenance</Text>
+    </View>
   );
 };
 
