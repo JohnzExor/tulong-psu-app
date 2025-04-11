62cf1e3800d13b177259688705b8d055d4893288
Huemman <mehmehkyuto@gmail.com>
2025-04-11 23:18:45 +0800

    add contents to the sidebar

--- app/components/sidebar/index.tsx
+++ app/components/sidebar/index.tsx
@@ -1,6 +1,11 @@
 import React from "react";
-import { View, Text, TouchableOpacity, Animated, Dimensions, TouchableWithoutFeedback } from "react-native";
+import { View, Text, TouchableOpacity, Animated, Dimensions, TouchableWithoutFeedback, Button } from "react-native";
 import AntDesign from '@expo/vector-icons/AntDesign';
+import { Link } from "expo-router";
+import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
+import Ionicons from '@expo/vector-icons/Ionicons';
+import Feather from '@expo/vector-icons/Feather';
+import SimpleLineIcons from '@expo/vector-icons/SimpleLineIcons';
 
 interface SidebarProps {
   isOpen: boolean;
@@ -9,7 +14,7 @@ interface SidebarProps {
 
 const Sidebar = ({ isOpen, onClose }: SidebarProps) => {
   const screenWidth = Dimensions.get("window").width;
-  const sidebarWidth = screenWidth * 0.75;
+  const sidebarWidth = screenWidth * 0.80;
   const animation = React.useRef(new Animated.Value(screenWidth)).current;
 
   React.useEffect(() => {
@@ -45,19 +50,62 @@ const Sidebar = ({ isOpen, onClose }: SidebarProps) => {
             <AntDesign name="close" size={24} color="black" />
           </TouchableOpacity>
         </View>
-        <Text className="text-2xl font-semibold text-foreground py-2">TULONG!</Text>
-        <TouchableOpacity className="mb-4">
-          <Text className="text-black text-base">Home</Text>
-        </TouchableOpacity>
-        <TouchableOpacity className="mb-4">
-          <Text className="text-black text-base">Profile</Text>
-        </TouchableOpacity>
-        <TouchableOpacity className="mb-4">
-          <Text className="text-black text-base">Settings</Text>
-        </TouchableOpacity>
-        <TouchableOpacity className="mb-4">
-          <Text className="text-black text-base">Logout</Text>
-        </TouchableOpacity>
+        <View>
+          <Text className="text-lg text-muted-foreground font-semibold">Reporting</Text>
+          <Link href={"/home/report"} onPress={onClose}>
+            <View className="flex-row items-center gap-3 rounded-lg p-3 text-sm duration-200 font-medium">
+              <MaterialCommunityIcons name="clipboard-plus-outline" size={24} color="black" />
+              <Text>Emergency Call</Text>
+            </View>
+          </Link>
+          <Link href={"/home/report/report-choices/campus-maintenance"} onPress={onClose}>
+              <View className="flex-row items-center gap-3 rounded-lg p-3 text-sm duration-200 font-medium">
+                <MaterialCommunityIcons name="clipboard-list-outline" size={24} color="black" />
+                <Text>Campus Maintenance</Text>
+              </View>
+          </Link>
+            <Link href={"/home/report/report-choices/handbook-violation"} onPress={onClose}>
+            <View className="flex-row items-center gap-3 rounded-lg p-3 text-sm duration-200 font-medium">
+                <MaterialCommunityIcons name="clipboard-text-outline" size={24} color="black" />
+                <Text>Handbook Violation</Text>
+            </View> 
+          </Link>
+        </View>
+        <View>
+          <Text className="text-lg text-muted-foreground font-semibold">Others</Text>
+          <Link href={"/home/report-progress"} onPress={onClose}>
+            <View className="flex-row items-center gap-3 rounded-lg p-3 text-sm duration-200 font-medium">
+              <Ionicons name="document-text-outline" size={24} color="black" />
+              <Text>Report Progress</Text>
+            </View>
+          </Link>
+          <Link href={"/home/handbook"} onPress={onClose}>
+              <View className="flex-row items-center gap-3 rounded-lg p-3 text-sm duration-200 font-medium">
+                <Feather name="book" size={24} color="black" />
+                <Text>Student Handbook</Text>
+              </View>
+          </Link>
+            <Link href={"/home/settings"} onPress={onClose}>
+            <View className="flex-row items-center gap-3 rounded-lg p-3 text-sm duration-200 font-medium">
+              <SimpleLineIcons name="settings" size={24} color="black" />
+              <Text>Settings</Text>
+            </View> 
+          </Link>
+        </View>
+        <View>
+          <Text className="text-lg text-muted-foreground font-semibold">Account</Text>
+          {/** Account HERE papi
+           */}
+        </View>
+        <View className="flex-1 justify-end w-[74%] ">
+          <TouchableOpacity 
+            onPress={() => console.log('Signed Out')} 
+            className="flex-row items-center justify-center border bg-background py-3 rounded-lg"
+          >
+            <MaterialCommunityIcons name="exit-to-app" size={24} color="black" />
+            <Text className="text-muted-foreground text-center font-bold">Sign out</Text>
+          </TouchableOpacity>
+        </View>
       </Animated.View>
 
       {isOpen && (
